6630a2df4f0926deaa77026a789c8709cdc8ab52
JahnaviSatishShanbhag <71307702+JahnaviSatishShanbhag@users.noreply.github.com>
2022-12-30 09:41:27 +0530

    Update paths_catalog.py

--- fcos_core/config/paths_catalog.py
+++ fcos_core/config/paths_catalog.py
@@ -7,15 +7,29 @@ import os
 class DatasetCatalog(object):
     DATA_DIR = "/kaggle/input/doclaynet"
     DATASETS = {
+        DATA_DIR = "datasets"
+        DATASETS = {
         "coco_2017_train": {
+            "img_dir": "coco/train2017",
+            "ann_file": "coco/annotations/instances_train2017.json"
+        },
+        "coco_2017_val": {
+            "img_dir": "coco/val2017",
+            "ann_file": "coco/annotations/instances_val2017.json"
+        },
+        "coco_2017_test_dev": {
+            "img_dir": "coco/test2017",
+            "ann_file": "coco/annotations/image_info_test-dev2017.json"
+        },
+        "doclaynet_train": {
             "img_dir": "PNG",
             "ann_file": "COCO/train.json"
         },
-        "coco_2017_val": {
+        "doclaynet_val": {
             "img_dir": "PNG",
             "ann_file": "COCO/val.json"
         },
-        "coco_2017_test_dev": {
+        "doclaynet_test": {
             "img_dir": "PNG",
             "ann_file": "COCO/test.json"
         },
